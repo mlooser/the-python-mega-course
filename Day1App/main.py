79a12ac27c21bb7c4c1bc0c99cd837ce2f780936
mlooser <mlooser@gmail.com>
2023-03-09 16:38:52 +0100

    base loop

--- Day1App/main.py
+++ Day1App/main.py
@@ -1,1 +1,15 @@
-print("Enter a TODO:")
+
+todos = []
+
+while True:
+    command = input("add/show/exit: ")
+
+    match command:
+        case 'add':
+            todo = input("What to add? ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+
